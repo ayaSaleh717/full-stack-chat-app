53aaa9fa112ab250693f7bb68eaae29e53f919dc
ayaSaleh717 <105351734+ayaSaleh717@users.noreply.github.com>
2025-09-05 13:22:58 +0300

    first commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:"http://localhost:3500/api" ,
+  baseURL:"https://chat-app-backend-iqah.onrender.com/api" ,
   withCredentials: true,
 });
