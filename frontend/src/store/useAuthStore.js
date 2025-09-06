aeec990294229b83d38c5b3cd6bcb709b3097cf4
ayaSaleh717 <105351734+ayaSaleh717@users.noreply.github.com>
2025-09-06 12:28:33 +0300

    first commit

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL =  "https://chat-app-backend-iqah.onrender.com" ;
+const BASE_URL =  "https://backend-chat-app-s30l.onrender.com/" ;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
